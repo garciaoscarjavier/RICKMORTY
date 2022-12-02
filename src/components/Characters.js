7fa8913d86ead28e3159fe9c8fc0c0824f8a3ed1
oscar garcia <garciaoscarjavier@gmail.com>
2022-12-02 12:11:27 -0500

    inicio de curso reack

---
+++ src/components/Characters.js
@@ -0,0 +1,65 @@
+
+export default function characters(props) {
+ 
+const { characters, setCharacters} = props;
+
+ const resetChrartes = () => {
+  setCharacters(null);
+
+ }
+
+return (
+
+    <div className="characters">
+    <h1>  characters desde   JS </h1>
+
+      <span className="back-home" onClick={resetChrartes}> Volver a la home</span>
+      <div className="container-characters">
+        {characters.map((character, index) => ( 
+           <div  className="character-container" key={index}>
+
+
+                  <div>
+                    <img src={character.image} alt={character.name} />
+
+                  </div>
+
+                      <div>
+                        <h3> {character.name}</h3>
+                        <h6>
+
+                            {character.status === "Alive" ? (
+                                <>
+                                <span className="alive" />
+                                  Alive
+                                </>
+                            
+                            ):(
+  
+                              <>
+                               <span className="dead" />
+                                Dead
+                              </> 
+    )}
+
+                        </h6>
+                              <p>
+
+                                <span className="text-grey"> Episodios: </span>
+                                <span>{character.episode.length}</span>
+                              </p>
+
+
+                        </div>
+
+          
+                    </div>
+        ))}
+
+
+      </div>
+      <span className="back-home" onClick={resetChrartes}> Volver a la home</span>
+
+     </div>
+  )
+}
